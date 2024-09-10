cfd60bec85e71ca3906b43acfb08480a519ba3cd
erwint <erwint@bii.a-star.edu.sg>
2024-09-10 14:13:21 +0800

    ETL for death table

--- etl/pypasar/omop/death.py
+++ etl/pypasar/omop/death.py
@@ -22,15 +22,66 @@ class death:
             raise err
 
     def initialize(self):
-        pass
+        with self.engine.connect() as connection:
+            with connection.begin():
+                # Set schema
+                connection.execute(
+                    text(f'SET search_path TO {os.getenv("POSTGRES_OMOP_SCHEMA")}'))
+
+                # Drop stg__death view if exists
+                connection.execute(text("DROP VIEW IF EXISTS stg__death"))
+
+                # Delete death table
+                connection.execute(text("TRUNCATE TABLE death"))
 
     def process(self):
-        # In batches
-        # Read from source
-        # Transform
-        # Ingest into OMOP Table
-        pass
+        # Set SCHEMA
+        omop_schema = os.getenv("POSTGRES_OMOP_SCHEMA")
+        source_schema = os.getenv("POSTGRES_SOURCE_SCHEMA")
+        omop_sqldev_schema = "omop_sqldev_schema"	### temporarily put this variable as the person table is empty in omop_etldev_schema
+
+        with self.engine.connect() as connection:
+            with connection.begin():
+                # Read from source and create a staging table (stg__death)
+                connection.execute(
+                    text(f'''CREATE OR REPLACE VIEW {omop_schema}.stg__death AS
+                        SELECT distinct
+                            cdm.person_id AS person_id,
+                            cdm.person_source_value AS person_source_value,
+                            source.death_date AS death_date,
+                            32879 AS death_type_concept_id
+                        FROM {source_schema}.info AS source
+                        JOIN {omop_sqldev_schema}.person AS cdm
+                            ON source.anon_case_no=cdm.person_source_value
+                        WHERE
+                            source.death_date IS NOT NULL'''
+                     ))
+
+                # Read from stg__death and insert into death
+                connection.execute(
+                    text(f'''
+                        INSERT INTO {omop_schema}.death (
+                            person_id,
+                            death_date,
+                            death_datetime,
+                            death_type_concept_id,
+                            cause_concept_id,
+                            cause_source_value,
+                            cause_source_concept_id
+                        )
+                        SELECT
+                            person_id,
+                            death_date,
+                            NULL AS death_datetime,
+                            death_type_concept_id,
+                            NULL AS cause_concept_id,
+                            NULL AS cause_source_value,
+                            NULL AS cause_source_concept_id
+                        FROM {omop_schema}.stg__death'''
+                     ))
 
     def finalize(self):
+        self.engine.dispose()
         # Verify if needed
         pass
+
