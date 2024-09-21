638b446d5211f1efb4737ac0d1a9f9ceff6c243e
nongnaphat <nongnaphat.wongpiyachai@gmail.com>
2024-09-21 10:00:22 +0000

    Add a number suffix to ensure visit_occurrence_id is unique

--- etl/pypasar/db/sql/postgres/visit_occurrence/stg__visit_occurrence.sql
+++ etl/pypasar/db/sql/postgres/visit_occurrence/stg__visit_occurrence.sql
@@ -6,6 +6,7 @@
 -- DATE        VERS  INITIAL  CHANGE DESCRIPTION
 -- ----------  ----  -------  ----------------------------------------
 -- 2024-09-14  1.00           Initial create
+-- 2024-09-21  2.00           Add a number suffix to ensure visit_occurrence_id is unique
 -- *******************************************************************
 
 -- Create the staging view for the visit_occurrence table
@@ -13,27 +14,28 @@ CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_occurrence AS
     -- Extract relevant columns from the pre_op.char table
     WITH preop__char AS (
         SELECT
-            session_id AS visit_occurrence_id,
             anon_case_no,
+            session_id,
             admission_type AS visit_source_value,
             session_startdate AS visit_start_date,
             session_enddate AS visit_end_date,
             institution_code
         FROM {PREOP_SCHEMA}.char
         -- To ensure unique rows
-        GROUP BY visit_occurrence_id, anon_case_no, visit_source_value, visit_start_date, visit_end_date, institution_code
+        GROUP BY anon_case_no, session_id, visit_source_value, visit_start_date, visit_end_date, institution_code
     ),
     -- Extract distinct relevant columns from the post_op.discharge table
     postop__discharge AS (
         SELECT DISTINCT
             anon_case_no,
+            session_id,
             external_hospital_code AS discharged_to_source_value
         FROM {POSTOP_SCHEMA}.discharge
     ),
     -- Combine the preop__char and postop__discharge
     final AS (
         SELECT
-            c.visit_occurrence_id AS visit_occurrence_id,
+            c.session_id AS session_id,
             c.anon_case_no AS anon_case_no, 
             c.visit_source_value AS visit_source_value, 
             c.visit_start_date AS visit_start_date, 
@@ -43,11 +45,23 @@ CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_occurrence AS
         FROM preop__char as c
         -- Ensure all records from preop__char are included, which will make up the person table
         LEFT JOIN postop__discharge AS d
+            -- Ensure only matching anon_case_no and session_id are included
             ON c.anon_case_no = d.anon_case_no
-        ORDER BY c.visit_occurrence_id
+            AND c.session_id = d.session_id
+        ORDER BY c.session_id
     )
     SELECT
-        visit_occurrence_id,
+        CASE
+            WHEN COUNT(*) OVER (PARTITION BY session_id) = 1 THEN
+                -- If the session_id is unique, concatenate it with '00'
+                CAST(session_id || '00' AS INTEGER)
+            ELSE
+                -- If not unique, concatenate with a padded row number
+                CAST(
+                    session_id || LPAD(CAST(ROW_NUMBER() OVER (PARTITION BY session_id ORDER BY visit_start_date) AS TEXT), 2, '0')
+                    AS INTEGER
+                )
+        END AS visit_occurrence_id,
         anon_case_no,               -- For mapping with the person_id field
         visit_source_value,         -- For mapping with the visit_concept_id field
         visit_start_date,
