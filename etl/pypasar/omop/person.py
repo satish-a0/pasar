44ce133d4972f1e55c779d0f5b1d606d56691697
lim.yz <yz.lim@novoheal.my>
2024-09-23 08:25:46 +0000

    Revert "🔀 Merge: Person Table (#4)"
    This reverts commit 8760163363991ab8468c9142dc27ad73b85620e8.

--- etl/pypasar/omop/person.py
+++ etl/pypasar/omop/person.py
@@ -3,9 +3,9 @@ import os
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from ..db.utils.postgres import postgres
-
 # Load environment variables from the .env file
 load_dotenv()
+
 
 class person:
 
@@ -22,45 +22,15 @@ class person:
             raise err
 
     def initialize(self):
-        with self.engine.connect() as connection:
-            with connection.begin():
-                # Set the schema for subsequent SQL operations
-                connection.execute(
-                    text(f'SET search_path TO {os.getenv("POSTGRES_OMOP_SCHEMA")}'))
-                # Drop the view if it exists
-                connection.execute(text("DROP VIEW IF EXISTS stg__person"))
-                # Clear all existing rows from the person table
-                connection.execute(text("TRUNCATE TABLE person"))
+        pass
 
     def process(self):
-        # List of SQL file paths
-        sql_files = [
-            os.path.join(os.getenv("BASE_PATH"), "person/stg__person.sql"),
-            os.path.join(os.getenv("BASE_PATH"), "person/person.sql")
-        ]
-        self.execute_sql_files(sql_files)
-
-    def execute_sql_files(self, file_paths):
-        # Define placeholder to environment variable mappings
-        placeholder_mapping = {
-            "{OMOP_SCHEMA}": os.getenv("POSTGRES_OMOP_SCHEMA"),
-            "{PREOP_SCHEMA}": os.getenv("POSTGRES_SOURCE_PREOP_SCHEMA")
-        }
-        
-        with self.engine.connect() as connection:
-            with connection.begin():
-                for file_path in file_paths:
-                    with open(file_path, 'r') as file:
-                        # Read the SQL script from the file
-                        sql_script = file.read()
-                        # Replace placeholders with actual values
-                        for placeholder, value in placeholder_mapping.items():
-                            if value is not None:
-                                sql_script = sql_script.replace(placeholder, value)
-                            else:
-                                raise ValueError(f"Environment variable for {placeholder} not set.")
-                        # Execute the SQL script
-                        connection.execute(text(sql_script))
+        # In batches
+        # Read from source
+        # Transform
+        # Ingest into OMOP Table
+        pass
 
     def finalize(self):
-        self.engine.dispose()
+        # Verify if needed
+        pass
