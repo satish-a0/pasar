ad3bb76f2d082f99b0e9fe820050d7f7f1c08254
satish-a0 <satish.anbazhagan@data4life-asia.care>
2024-09-30 13:50:28 +0000

    Load v0.1 data

--- etl/pypasar/omop/source_to_concept_map.py
+++ etl/pypasar/omop/source_to_concept_map.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from ..db.utils.postgres import postgres
+import pandas as pd
 # Load environment variables from the .env file
 load_dotenv()
 
@@ -11,6 +12,9 @@ class source_to_concept_map:
 
     def __init__(self):
         self.engine = postgres().get_engine()  # Get PG Connection
+        self.omop_schema = os.getenv("POSTGRES_OMOP_SCHEMA")
+        self.source_file = os.path.join(os.getenv("BASE_PATH"), "source_to_concep_map", "v0.1", "data.csv")
+        self.temp_table = f'temp_source_to_concept_map_{os.urandom(15).hex()}'
 
     def execute(self):
         try:
@@ -22,15 +26,32 @@ class source_to_concept_map:
             raise err
 
     def initialize(self):
-        pass
+        # Truncate
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(text(f"Truncate table {self.omop_schema}.source_to_concept_map"))
 
     def process(self):
-        # In batches
-        # Read from source
-        # Transform
-        # Ingest into OMOP Table
+        self.retrieve()
+        self.ingest()
+
+
+    def retrieve(self):
+        df = pd.read_csv(self.source_file)
+        df.drop(["target_vocabulary_id", "invalid_reason"], inplace=True, axis=1)
+        # print(df.head(2))
+        df.to_sql(name=self.temp_table, schema=self.omop_schema, con=self.engine, if_exists='replace',index=False)
+
+    def ingest(self):
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(
+                    text(f'SET search_path TO {self.omop_schema}'))
+                connection.execute(text(f"INSERT INTO source_to_concept_map (source_code, source_concept_id,source_vocabulary_id,source_code_description, target_concept_id, target_vocabulary_id, invalid_reason) SELECT t.source_code, t.source_concept_id, t.source_vocabulary_id, t.source_code_description, t.target_concept_id, c.vocabulary_id, c.invalid_reason FROM {self.temp_table} t INNER JOIN concept c ON c.concept_id = t.target_concept_id"))
         pass
 
     def finalize(self):
-        # Verify if needed
-        pass
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(text(f"Drop table {self.omop_schema}.{self.temp_table}"))
+        self.engine.dispose()
