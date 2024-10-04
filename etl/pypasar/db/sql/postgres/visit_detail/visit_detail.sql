3839f11505fb7cc5dd06cfa91301b1a9d1f039b1
Nongnaphat Wongpiyachai <nongnaphat.wongpiyachai@gmail.com>
2024-10-04 11:24:00 +0700

    🔀 Merge: Visit Detail Table (#18)
    * Replace psycopg2 with psycopg2-binary
    
    * Initiate SQL template for Person table
    
    * Update staging view to use DENSE_RANK() for person_id
    
    * Insert data into  table from staging view
    
    * Add person.md
    
    * Update person.md
    
    * Replace pre_op.char with {PRE_OP_SCHEMA}.char
    
    * Update the schema name
    
    * Modify to handle processing of SQL files for the Person table
    
    * Update race_concept_id
    
    * Update the schema name
    
    * Updated CTEs to use filteredSource and prioritized non-null data selection
    
    * Assign person_id based on the ordering of person_source_value
    
    * Update to assign person_id by selecting from the earliest for each anon_case_no
    
    * Update person.md
    
    * Update the schema name
    
    * Update to use filteredSource data instead of source
    
    * Revert "Replace psycopg2 with psycopg2-binary"
    
    This reverts commit 254328270e549b4087fdb950cdf0f8eabde435ae.
    
    * add table visit_detail
    
    * Remove unnecessary file
    
    * Ensure only distinct rows from postop__icu source
    
    * Add filter to select rows where enddate is not null, as it is a required field
    
    * Update staging view
    
    * Remove generate visit_detail_id from staging view
    
    * Create the initial intermediate view for the visit_detail table
    
    * Update schema name
    
    * Concatenate date and time fields
    
    * Add id field to staging view
    
    * Update intermediate view
    
    * Process final visit_detail table
    
    * Enhance path building for SQL scripts
    
    * Fix spacing errors
    
    * Add visit_detail.md
    
    * Update visit_detail.md
    
    * Update visit_detail.md
    
    * Ignore the intra_op.operation table
    
    * Remove the mapping with the provider table
    
    * Set provider ID to NULL
    
    * Update visit_detail.md
    
    ---------
    
    Co-authored-by: Chinapat Onprasert <chinapat.onp@mahidol.edu>

---
+++ etl/pypasar/db/sql/postgres/visit_detail/visit_detail.sql
@@ -0,0 +1,54 @@
+-- *******************************************************************
+-- NAME: visit_detail.sql
+-- DESC: Final table - visit_detail
+-- *******************************************************************
+-- CHANGE LOG:
+-- DATE        VERS  INITIAL  CHANGE DESCRIPTION
+-- ------ ----  ----  -------  ----------------------------------------
+-- 2024-09-24  1.00            Initial create
+-- 2024-10-01  2.00            Set provider ID to NULL
+-- *******************************************************************
+
+INSERT INTO {OMOP_SCHEMA}.visit_detail (
+    visit_detail_id,
+    person_id,
+    visit_detail_concept_id,
+    visit_detail_start_date,
+    visit_detail_start_datetime,
+    visit_detail_end_date,
+    visit_detail_end_datetime,
+    visit_detail_type_concept_id,
+    provider_id,
+    care_site_id,
+    visit_detail_source_value,
+    visit_detail_source_concept_id,
+    admitted_from_concept_id,
+    admitted_from_source_value,
+    discharged_to_source_value,
+    discharged_to_concept_id,
+    preceding_visit_detail_id,
+    parent_visit_detail_id,
+    visit_occurrence_id
+)
+SELECT
+    -- Assign a unique visit_detail_id to each combination of session_startdate and id
+    ROW_NUMBER() OVER (ORDER by session_startdate, id) AS visit_detail_id,
+    person_id,
+    32037 AS visit_detail_concept_id, 
+    visit_detail_start_date,
+    visit_detail_start_datetime,
+    visit_detail_end_date,
+    visit_detail_end_datetime,
+    32879 AS visit_detail_type_concept_id, 
+    NULL AS provider_id,
+    care_site_id,
+    'ICU'::text AS visit_detail_source_value,
+    NULL AS visit_detail_source_concept_id,
+    NULL AS admitted_from_concept_id,
+    NULL AS admitted_from_source_value,
+    NULL AS discharged_to_source_value,
+    NULL AS discharged_to_concept_id,
+    NULL AS preceding_visit_detail_id,
+    NULL AS parent_visit_detail_id,
+    visit_occurrence_id
+FROM {OMOP_SCHEMA}.int__visit_detail;
