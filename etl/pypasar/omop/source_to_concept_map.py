ea786e9baccb5d39a4f10fe03a2acc86719afeda
satish-a0 <satish.anbazhagan@data4life-asia.care>
2024-10-07 23:02:19 +0000

    Increase version & fix valid start, end dates

--- etl/pypasar/omop/source_to_concept_map.py
+++ etl/pypasar/omop/source_to_concept_map.py
@@ -13,7 +13,7 @@ class source_to_concept_map:
     def __init__(self):
         self.engine = postgres().get_engine()  # Get PG Connection
         self.omop_schema = os.getenv("POSTGRES_OMOP_SCHEMA")
-        self.source_file = os.path.join(os.getenv("BASE_PATH"), "source_to_concep_map", "v0.1", "data.csv")
+        self.source_file = os.path.join(os.getenv("BASE_PATH"), "source_to_concep_map", "v2", "data.csv")
         self.temp_table = f'temp_source_to_concept_map_{os.urandom(15).hex()}'
 
     def execute(self):
@@ -47,7 +47,7 @@ class source_to_concept_map:
             with connection.begin():
                 connection.execute(
                     text(f'SET search_path TO {self.omop_schema}'))
-                connection.execute(text(f"INSERT INTO source_to_concept_map (source_code, source_concept_id,source_vocabulary_id,source_code_description, target_concept_id, target_vocabulary_id, invalid_reason) SELECT t.source_code, t.source_concept_id, t.source_vocabulary_id, t.source_code_description, t.target_concept_id, c.vocabulary_id, c.invalid_reason FROM {self.temp_table} t INNER JOIN concept c ON c.concept_id = t.target_concept_id"))
+                connection.execute(text(f"INSERT INTO source_to_concept_map (source_code, source_concept_id,source_vocabulary_id,source_code_description, target_concept_id, target_vocabulary_id, invalid_reason, valid_start_date, valid_end_date) SELECT t.source_code, t.source_concept_id, t.source_vocabulary_id, t.source_code_description, t.target_concept_id, c.vocabulary_id, c.invalid_reason, c.valid_start_date, c.valid_end_date FROM {self.temp_table} t INNER JOIN concept c ON c.concept_id = t.target_concept_id"))
         pass
 
     def finalize(self):
