5b315be7faa03f91e2b743926b9e9f08ce4fb93a
satish-a0 <satish.anbazhagan@data4life-asia.care>
2024-10-07 22:04:07 +0000

    remove unused

--- etl/pypasar/omop/measurement.py
+++ etl/pypasar/omop/measurement.py
@@ -12,7 +12,7 @@ load_dotenv()
 class measurement():
 
     def __init__(self):
-        self.Source = Enum(value='Source', names=[("PREOP_LAB", f"{os.getenv('POSTGRES_SOURCE_PREOP_SCHEMA')}.lab"), 
+        self.source = Enum(value='Source', names=[("PREOP_LAB", f"{os.getenv('POSTGRES_SOURCE_PREOP_SCHEMA')}.lab"), 
                                                   ("PREOP_CHAR", f"{os.getenv('POSTGRES_SOURCE_PREOP_SCHEMA')}.char"),
                                                   ("INTRAOP_AIMSVITALS", f"{os.getenv('POSTGRES_SOURCE_INTRAOP_SCHEMA')}.aimsvitals"),
                                                   ("INTRAOP_OPERATION", f"{os.getenv('POSTGRES_SOURCE_INTRAOP_SCHEMA')}.operation"),
@@ -27,53 +27,53 @@ class measurement():
         self.source_intraop_schema = os.getenv("POSTGRES_SOURCE_INTRAOP_SCHEMA")
         self.source_postop_schema = os.getenv("POSTGRES_SOURCE_POSTOP_SCHEMA")
         self.temp_table = f"temp_measurement_preop_lab_{os.urandom(15).hex()}"
-        self.measurement_id_start, self.measurement_id_end = 1, 0
-        self.source_tables_cols = [{"table": f"{self.source_preop_schema}.lab", 
+        self.measurement_id_start = 1
+        self.source_tables_cols = [{"table": self.source.PREOP_LAB.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "preop_lab_test_description": str,
                                               "preop_lab_result_value": float, 
                                               "preop_lab_collection_datetime": "datetime64[ns]"}},
-                                   {"table": f"{self.source_preop_schema}.char", 
+                                   {"table": self.source.PREOP_CHAR.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "session_startdate": "datetime64[ns]",
                                               "height": float, "weight": float, "bmi": float, "systolic_bp": float,
                                               "diastolic_bp": float, "heart_rate": float,
                                               "o2_saturation": float, "o2_supplementaries": str,
                                               "temperature": float, "pain_score": float}}, 
-                                   {"table": f"{self.source_intraop_schema}.aimsvitals", 
+                                   {"table": self.source.INTRAOP_AIMSVITALS.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "vitalcode": str,
                                               "vital_num_value": float, 
                                               "vitaldt": "datetime64[ns]", "vital_date": "datetime64[ns]", "vital_time": str}}, 
-                                   {"table": f"{self.source_intraop_schema}.operation", 
+                                   {"table": self.source.INTRAOP_OPERATION.value, 
                                     "columns": {"anon_case_no": str, "vital_code": str,
                                               "vital_signs_result": float, "vital_signs_taken_datetime": "datetime64[ns]",
                                               "vital_signs_taken_date": "datetime64[ns]", 
                                               "vital_signs_taken_time": str}}, 
-                                   {"table": f"{self.source_postop_schema}.lab", 
+                                   {"table": self.source.POSTOP_LAB.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, 
                                               "postop_lab_collection_datetime_max": "datetime64[ns]",
                                               "postop_lab_collection_datetime_min": "datetime64[ns]", 
                                               "postop_lab_test_desc": str,
                                               "postop_result_value_max": float, "postop_result_value_min": float}}, 
-                                   {"table": f"{self.source_postop_schema}.labsall", 
+                                   {"table": self.source.POSTOP_LABSALL.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "gen_lab_lab_test_code": str,
                                               "gen_lab_result_value": str, "gen_lab_specimen_collection_date": "datetime64[ns]",
                                               "gen_lab_specimen_collection_time": str}},
-                                   {"table": f"{self.source_preop_schema}.others", 
+                                   {"table": self.source.PREOP_OTHERS.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "session_startdate": "datetime64[ns]",
                                               "asa_score_aims": float, "asa_score_eaf": str,
                                               "efs_total_score": int}}, 
-                                   {"table": f"{self.source_preop_schema}.riskindex", 
+                                   {"table": self.source.PREOP_RISKINDEX.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "session_id": int, "session_startdate": "datetime64[ns]",
                                               "asa_class": str, "cri_functional_status": str,
                                               "cardiac_risk_index": float, "cardiac_risk_class": str,
                                               "osa_risk_index": str, "act_risk": str}}, 
-                                   {"table": f"{self.source_intraop_schema}.nurvitals", 
+                                   {"table": self.source.INTRAOP_NURVITALS.value, 
                                     "columns": {"anon_case_no": str, "id": int,
                                               "authored_datetime": "datetime64[ns]", "document_item_desc": str, "document_item_right_label": str, "value_text": str}}]
 
@@ -94,7 +94,7 @@ class measurement():
 
     def process(self):
         for source_table_cols in self.source_tables_cols:
-            if source_table_cols["table"] in [f"{self.source_preop_schema}.lab", f"{self.source_intraop_schema}.aimsvitals"]:
+            if source_table_cols["table"] in [f"{self.source_preop_schema}.lab"]:
                 print(source_table_cols)
                 self.limit, self.offset = int(os.getenv("PROCESSING_BATCH_SIZE")), 0
                 self.process_by_source_table(source_table_cols)
@@ -106,10 +106,10 @@ class measurement():
         print(f"Total count {total_count_source_table}")
         while self.offset <= total_count_source_table: # Fetch and process in batches
             source_batch = self.retrieve(source_table_cols)
-            self.measurement_id_end += len(source_batch)
-            #print(f"measurement id start: {self.measurement_id_start} measurement id end: {self.measurement_id_end}")
+            print(f"measurement id start: {self.measurement_id_start}")
             transformed_batch = self.transform(source_table_cols, source_batch)
             self.measurement_id_start += len(source_batch)
+            # break
             del source_batch
             self.ingest(transformed_batch)
             del transformed_batch
@@ -141,7 +141,8 @@ class measurement():
                 select_sql += f"{col},"
             else:
                 select_sql += f"{col}"
-        select_sql += f" FROM {source_table_cols['table']} LIMIT {self.limit} OFFSET {self.offset}"
+        select_sql += f" FROM {source_table_cols['table']} order by anon_case_no LIMIT {self.limit} OFFSET {self.offset}"
+        # select_sql += f" FROM {source_table_cols['table']} order by anon_case_no LIMIT 1"
         #print(select_sql)
         with self.engine.connect() as connection:
             with connection.begin():
@@ -157,7 +158,7 @@ class measurement():
             "measurement_date": "datetime64[ns]",
             "measurement_datetime": "datetime64[ns]",
             "measurement_time": str,
-            "value_as_number": float,
+            "value_as_number": object,
             "measurement_source_value": str,
             "value_source_value": str,
             "visit_occurrence_id": int,
@@ -167,23 +168,23 @@ class measurement():
         measurement_df = pd.DataFrame(columns=measurement_schema.keys()).astype(measurement_schema)
 
         match source_table_cols["table"]:
-            case self.Source.PREOP_LAB.value:
+            case self.source.PREOP_LAB.value:
                 return self.transform_preop_lab(source_table_cols, source_batch, measurement_df)
-            case self.Source.PREOP_CHAR.value:
+            case self.source.PREOP_CHAR.value:
                 return self.transform_preop_char(source_table_cols, source_batch, measurement_df)
-            case self.Source.INTRAOP_AIMSVITALS.value:
+            case self.source.INTRAOP_AIMSVITALS.value:
                 return self.transform_intraop_aimsvitals(source_table_cols, source_batch, measurement_df)
-            case self.Source.INTRAOP_OPERATION.value:
+            case self.source.INTRAOP_OPERATION.value:
                 return self.transform_intraop_operation(source_table_cols, source_batch, measurement_df)
-            case self.Source.POSTOP_LAB.value:
+            case self.source.POSTOP_LAB.value:
                 return self.transform_postop_lab(source_table_cols, source_batch, measurement_df)
-            case self.Source.POSTOP_LABSALL.value:
+            case self.source.POSTOP_LABSALL.value:
                 return self.transform_postop_labsall(source_table_cols, source_batch, measurement_df)
-            case self.Source.PREOP_OTHERS.value:
+            case self.source.PREOP_OTHERS.value:
                 return self.transform_preop_others(source_table_cols, source_batch, measurement_df)
-            case self.Source.PREOP_RISKINDEX.value:
+            case self.source.PREOP_RISKINDEX.value:
                 return self.transform_preop_riskindex(source_table_cols, source_batch, measurement_df)
-            case self.Source.INTRAOP_NURVITALS.value:
+            case self.source.INTRAOP_NURVITALS.value:
                 return self.transform_intraop_nurvitals(source_table_cols, source_batch, measurement_df)
             case _:
                 raise ValueError(f"Invalid source table {source_table_cols['table']}")
@@ -196,33 +197,38 @@ class measurement():
             measurement_df["measurement_type_concept_id"] = 32879
             measurement_df["value_as_number"] = source_batch["preop_lab_result_value"]
             measurement_df["measurement_source_value"] = source_batch["preop_lab_test_description"]
-            measurement_df["measurement_id"] = range(self.measurement_id_start, self.measurement_id_end + 1)
+            measurement_df["measurement_id"] = range(self.measurement_id_start, (self.measurement_id_start + len(measurement_df)))
 
             measurement_df["value_source_value"] = None # TODO: Update Vocab concept id for "preop_lab_result_value"
             measurement_df["measurement_concept_id"] = 0 # TODO: Update
             measurement_df["person_id"] = 0 # TODO: Update
             measurement_df["visit_occurrence_id"] = 0 # TODO: Update
 
-        # print(measurement_df.head(1))
+        print(measurement_df.head(100))
         return measurement_df
 
     def transform_preop_char(self, source_table_cols, source_batch, measurement_df):
         print(f"INSIDE transform_preop_char..")
-        # if len(source_batch) > 0:
-        #     measurement_df["measurement_date"] = pd.to_datetime(source_batch["preop_lab_collection_datetime"]).dt.date
-        #     measurement_df["measurement_datetime"] = source_batch["preop_lab_collection_datetime"]
-        #     measurement_df["measurement_type_concept_id"] = 32879
-        #     measurement_df["value_as_number"] = source_batch["preop_lab_result_value"]
-        #     measurement_df["measurement_source_value"] = source_batch["preop_lab_test_description"]
-        #     measurement_df["measurement_id"] = range(self.measurement_id_start, self.measurement_id_end + 1)
+        # combined_list = [source_batch["height"], source_batch["weight"], source_batch["bmi"],
+        #                                           source_batch["systolic_bp"], source_batch["diastolic_bp"], source_batch["heart_rate"], source_batch["o2_saturation"], source_batch["o2_supplementaries"], source_batch["temperature"], source_batch["pain_score"]]
+        # combined_list= [1,2,3,4,5,6,7]
+        # 1 to Many
+        if len(source_batch) > 0:
+            measurement_df["measurement_date"] = pd.to_datetime(source_batch["session_startdate"]).dt.date
+            measurement_df["measurement_type_concept_id"] = 32879
+            # measurement_df["measurement_source_value"] = source_batch[""]
+            value_as_number_df = source_batch[["height","weight","bmi", "systolic_bp", "diastolic_bp", "heart_rate", "o2_saturation", "o2_supplementaries", "temperature", "pain_score"]]
+            # measurement_df["anon_case_no"] = source_batch["anon_case_no"]
+            measurement_df["value_as_number"] = value_as_number_df.apply(lambda row: row.tolist(), axis=1)
 
-        #     measurement_df["value_source_value"] = None # TODO: Update Vocab concept id for "preop_lab_result_value"
-        #     measurement_df["measurement_concept_id"] = 0 # TODO: Update
-        #     measurement_df["person_id"] = 0 # TODO: Update
-        #     measurement_df["visit_occurrence_id"] = 0 # TODO: Update
-
-        # print(measurement_df.head(1))
-        return measurement_df
+            # measurement_df["value_source_value"] = None # TODO: Update Vocab concept id for "preop_lab_result_value"
+            measurement_df["measurement_concept_id"] = 0 # TODO: Update
+            measurement_df["visit_occurrence_id"] = 0 # TODO: Update 
+                    
+        new_measurement_df = measurement_df.explode("value_as_number")
+        new_measurement_df["measurement_id"] = range(self.measurement_id_start, (self.measurement_id_start + len(new_measurement_df)))
+        print(new_measurement_df.head(len(new_measurement_df)))
+        return new_measurement_df
 
     def transform_intraop_aimsvitals(self, source_table_cols, source_batch, measurement_df):
         print(f"INSIDE transform_intraop_aimsvitals..")
@@ -233,8 +239,7 @@ class measurement():
             measurement_df["measurement_type_concept_id"] = 32879
             measurement_df["value_as_number"] = source_batch["vital_num_value"]
             measurement_df["measurement_source_value"] = source_batch["vitalcode"]
-            measurement_df["measurement_id"] = range(self.measurement_id_start, self.measurement_id_end + 1)
-
+            measurement_df["measurement_id"] = range(self.measurement_id_start, (self.measurement_id_start + len(measurement_df)))
             measurement_df["measurement_concept_id"] = 0 # TODO: Update
             measurement_df["person_id"] = 0 # TODO: Update
             measurement_df["visit_occurrence_id"] = 0 # TODO: Update
